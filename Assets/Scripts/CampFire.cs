8e9965e74ab9061a3977475b9229afb2da0584f0
Lee Sanghun <hun1387s@gmail.com>
2025-03-07 15:47:22 +0900

    점프대 구현

--- Assets/Scripts/CampFire.cs
+++ Assets/Scripts/CampFire.cs
@@ -7,6 +7,7 @@ public class CampFire : MonoBehaviour
     public int damage;
     public float damageRate;
     public float delayTime = 0;
+    bool enter = false;
 
     HashSet<IDamagable> things = new HashSet<IDamagable>(); // 중복 방지
 
@@ -27,7 +28,7 @@ public class CampFire : MonoBehaviour
     {
         yield return new WaitForSeconds(delayTime);
 
-        if (!things.Contains(damagable)) // 여전히 범위 내에 있는지 확인
+        if (!things.Contains(damagable) && enter)
         {
             things.Add(damagable);
         }
@@ -35,6 +36,7 @@ public class CampFire : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        enter = true;
         if (other.TryGetComponent(out IDamagable damagable))
         {
             StartCoroutine(AddDelay(damagable));
@@ -43,6 +45,7 @@ public class CampFire : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
+        enter = false;
         if (other.TryGetComponent(out IDamagable damagable))
         {
             things.Remove(damagable);
---
+++ Assets/Scripts/JumpBlock.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpBlock : MonoBehaviour
+{
+    public float jumpForce = 10f;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("점프대 진입");
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("캐릭터 진입");
+            Rigidbody _rigidbody = other.GetComponent<Rigidbody>();
+            if (_rigidbody != null)
+            {
+                // 위쪽으로 힘을 가한다.
+                _rigidbody.velocity = new Vector3(_rigidbody.velocity.x, 0f, _rigidbody.velocity.z);
+                _rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            }
+
+        }
+    }
+
+}
